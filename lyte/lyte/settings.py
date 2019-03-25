bcb37e6eb4bf7b7af7ee037b8402eca7e412d43d
Inna Shuster <inna_shuster@Innas-MacBook-Pro.local>
2019-03-25 10:38:49 -0700

    Update

--- lyte/lyte/settings.py
+++ lyte/lyte/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'events',
-    'rest_framework'
+    'rest_framework',
+    'gunicorn',
+    'django_heroku'
 ]
 
 MIDDLEWARE = [
