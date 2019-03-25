85b5878a011f409a667fdd7ac3c0ced4a7e215e5
Inna Shuster <inna_shuster@Innas-MacBook-Pro.local>
2019-03-25 01:15:00 -0700

    Ready to heroku this sucker in the face.

--- lyte/events/serializers.py
+++ lyte/events/serializers.py
@@ -5,4 +5,3 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event 
         fields = ('id','name', 'start_date', 'organizer_id', 'ticket_cost','created','last_updated')
-        # fields = ('id','name', 'start_date', 'organizer_name', 'ticket_cost','created','last_updated')
--- lyte/events/views.py
+++ lyte/events/views.py
@@ -12,7 +12,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-# from django.forms.models import model_to_dict
 from django.http import Http404
 from django.views.decorators.csrf import csrf_exempt
 
