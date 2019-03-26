b4650c264f65045c8e6ecebf98318422ec022da2
Inna Shuster <inna_shuster@Innas-MacBook-Pro.local>
2019-03-25 17:42:25 -0700

    wasgi

--- lyte/lyte/wsgi.py
+++ lyte/lyte/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lyte.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lyte.lyte.settings')
 
 application = get_wsgi_application()
